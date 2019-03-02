44e1e75f977430b6d41d925c62165fa801376ad4
zygzzp <1350202075@qq.com>
2019-03-02 18:24:24 +0800

    my first commit ,creat geli by interface

---
+++ src/com/chapter04/Moattack.java
@@ -0,0 +1,18 @@
+package com.chapter04;
+interface Geli{
+    public void responseAsk();
+}
+class LiuDeHua implements Geli{
+    public LiuDeHua() {
+    }
+    @Override
+    public void responseAsk() {
+        System.out.println("墨者革离");
+    }
+}
+public class Moattack {
+    public static void main(String[] args) {
+        Geli geli = new LiuDeHua();
+        geli.responseAsk();
+    }
+}
